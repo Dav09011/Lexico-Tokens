241af1156bedd6878ba82f27b25198684027f8f1
Dav09011 <l21140736@queretaro.tecnm.mx>
2023-09-05 16:24:41 -0600

    first commit

---
+++ Program (4).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LYA1_Lexico
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            try
+            {
+                using (Lexico L = new Lexico())
+                {
+                    L.setContenido("radio");
+                    L.setClasificacion(Token.Tipos.Identificador);
+
+                    Console.WriteLine(L.getContenido());
+                    Console.WriteLine(L.getClasificacion());
+                }
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e.Message);
+            }
+        }
+    }
+}
